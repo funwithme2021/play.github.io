886f54daef27b9ee87793fefc80d176ad45b7b83
funwithme2021 <125599198+funwithme2021@users.noreply.github.com>
2026-03-20 21:03:53 +0800

    Delete 43rd中原會計系系代會檔案上傳與查詢系統 directory

--- 43rd中原會計系系代會檔案上傳與查詢系統/marquee.js
+++
@@ -1,6 +0,0 @@
-  // 跑马灯的文本内容
-  const marqueeText = 
-  "◉歡迎來到43rd中原會計系系學會檔案上傳與查詢上傳系統，電腦與手機版本網頁同步上線中，歡迎使用。 ◉每日13-14時及每週六0-6時為維護時段，暫停使用。 ◉112年11月22日21時0分，於中原大學管理大樓401室辦理1121學年度11月份系代會，請應出席委員準時出席。  ◉112年11月27日21時0分，於中原大學商學大樓系聯辦理1121學年度11月份結帳，請應出席委員準時出席。";
-
-  const marqueeContentElement = document.getElementById('marquee-content');
-  marqueeContentElement.textContent = marqueeText;
